f3b2ed298dcf62ec953a207413bcdbaf1802c4e3
Gabor Javorszky <g.javorszky@f5.com>
2024-04-23 16:48:15 +0100

    Restore dockerfiles to previous setting

--- docker/make.mk
+++ docker/make.mk
@@ -4,10 +4,10 @@
 # PR 1162 needs to be merged, and Unit 1.33 needs to be tagged (assuming 1.33 has the
 # changes in it).
 build-unit:
-	docker build -f unit-requesturi.dockerfile -t unit:local-php .
+	docker build -f unit-requesturi.dockerfile --no-cache -t unit:local-php .
 
 build-wp:
-	docker build -f unit-wp.dockerfile -t unit:wp-ms .
+	docker build -f unit-wp.dockerfile --no-cache -t unit:wp-ms .
 
 build: build-unit build-wp
 	
